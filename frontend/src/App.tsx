bf779f05a065e09bc4a65fe320d348d37dcf98a6
sid0000007 <mr.siddharth9267963125@gmail.com>
2024-09-06 20:32:22 +0530

    auth setup || backend arc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <header className=" justify-center flex item-center text-center">
+      <h1 className="">Insurgence</h1>
+      <p>Insurgence is a web-based game that is currently in development.</p>
+      <p>It is a fan-made project and is not affiliated with Nintendo.</p>
+      <p>For more information, please visit the <a href='/'>Insurgence website</a>.</p>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
 
-export default App
+
+   
