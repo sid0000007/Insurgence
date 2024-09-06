1ea34c674d540230be58f9fb20226ffc85d0bb5d
sid0000007 <mr.siddharth9267963125@gmail.com>
2024-09-06 20:46:33 +0530

    asdadad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,22 @@ import { BentoDemo } from "./components/custom/BentoGrade";
 
 export default function App() {
   return (
-    <header className=" justify-center flex item-center text-center">
-      <h1 className="">Insurgence</h1>
-      <p>Insurgence is a web-based game that is currently in development.</p>
-      <p>It is a fan-made project and is not affiliated with Nintendo.</p>
-      <p>For more information, please visit the <a href='/'>Insurgence website</a>.</p>
+    
+    <>
+    <h1 className="">Insurgence</h1>
+    <p>Insurgence is a web-based game that is currently in development.</p>
+    <p>It is a fan-made project and is not affiliated with Nintendo.</p>
+    <p>For more information, please visit the <a href='/'>Insurgence website</a>.</p>
+   
+     <BentoDemo/>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </header>
+      </>
+   
   );
 }
 
